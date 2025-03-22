165fb220db21dc59a115a688d3b198ae699fca10
leonburster <leon.burster@icloud.com>
2025-03-22 10:02:09 -0700

    - added static Variable customerCount to Customer class - added unit test, that tests for the maximum of 100 Customer objects

--- src/people/Customer.java
+++ src/people/Customer.java
@@ -5,27 +5,32 @@ public class Customer extends Member {
     public enum Goal {looseWeight,gainMuscles,stayHealthy}
     private Goal goal;
     private final int activityLevel;
+    private static int customerCount;
 
     public Goal getGoal() {return goal;}
     public int getActivityLevel() {return activityLevel;}
 
     public Customer(String name, int age, String gender, double height, double weight, int id, Goal goal, int activityLevel) {
         super(name, age, gender, height, weight, id);
-
+        if (customerCount>100)
+            throw new RuntimeException("Customer count exceeds 100");
         if (Utility.isNullOrWhiteSpace(goal.toString()))
             throw new IllegalArgumentException("Goal cannot be null or empty");
         if (!(goal==Goal.looseWeight || goal==Goal.gainMuscles || goal==Goal.stayHealthy))
-            throw new RuntimeException("Goal must be low or medium or high");
+            throw new RuntimeException("Goal must be looseWeight or gainMuscles or stayHealthy");
         this.goal = goal;
         if(!(activityLevel>0 && activityLevel<8))
             throw new RuntimeException("Activity must be between 1 and 7");
         this.activityLevel = activityLevel;
+        customerCount++;
     }
 
     public double getBMI() {
         return getWeight()/(getHeight()*getHeight());
     }
     public void updateGoal (Goal newGoal) {
+        if (customerCount>100)
+            throw new RuntimeException("Customer count exceeded 100");
         if (newGoal == this.goal)
             throw new RuntimeException("Goal is already set to " + this.goal);
         if (Utility.isNullOrWhiteSpace(newGoal.toString()))
@@ -33,6 +38,7 @@ public class Customer extends Member {
         if (!(newGoal==Goal.looseWeight || newGoal==Goal.gainMuscles || newGoal==Goal.stayHealthy))
             throw new RuntimeException("Goal must be low or medium or high");
         this.goal = newGoal;
+        customerCount++;
     }
 
     //needs to be implemented
